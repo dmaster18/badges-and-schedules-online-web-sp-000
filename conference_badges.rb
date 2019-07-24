2a42b97fd6cda0f22c21b44f3462384471be9533
David Strauch <dmaster18@gmail.com>
2019-07-24 15:45:37 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -21,10 +21,10 @@ def assign_rooms(speakers)
   rooms = []
   counter = 0 
   speakers.each do |name|
-    rooms
+        counter+=1
     room_assignment = "Hello, #{name}! You'll be assigned to room\ #{rooms[counter]}"
     array_of_room_assignments.push(room_assignment)
-    counter+=1
+
   end
   array_of_room_assignments
 end
