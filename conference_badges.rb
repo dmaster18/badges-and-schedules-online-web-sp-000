2fc814e724e63d062ad4afdd4fd14ffabfd3abb1
David Strauch <dmaster18@gmail.com>
2019-07-24 15:01:59 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,7 +1,7 @@
 # Write your code here.
 array_of_names = ["Edsger","Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
-def badge_maker(array_of_names)
+def badge_maker(name)
   puts "Hello, my name is #{name}."
 end
 
