0cfdac8186cad81f64e51e2dd1951ba612e9f999
David Strauch <dmaster18@gmail.com>
2019-07-24 15:12:17 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,7 +5,7 @@ SPEAKERS = ["Edsger","Ada", "Charles", "Alan", "Grace", "Linus",
 ROOMS = [1, 2, 3, 4, 5, 6, 7]
 
 def badge_maker(name)
-  puts "Hello, my name is #{name}."
+  "Hello, my name is #{name}."
 end
 
 def batch_badge_creator(SPEAKERS)
