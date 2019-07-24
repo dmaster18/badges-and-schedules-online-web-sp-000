0a5d088bb74569e834f0b75d9b42fc7a16fa11f8
David Strauch <dmaster18@gmail.com>
2019-07-24 15:10:23 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,7 +13,7 @@ def batch_badge_creator(array_of_names)
   array_of_badge_messages 
 end
 
-rooms = [1, 2, 3, 4, 5, 6, 7]
+ROOMS = [1, 2, 3, 4, 5, 6, 7]
 
 def assign_rooms(array_of_names)
   array_of_room_assignments = []
